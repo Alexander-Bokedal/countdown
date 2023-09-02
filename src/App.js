c90a536f600fe43c43edd00ef280601d07def98b
Alexander Bokedal <alexander.bokedal@gmail.com>
2023-09-02 09:57:56 +0200

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,58 @@
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
 import './App.css';
 
 function App() {
+  const targetDate = new Date("February 1, 2024 00:00:00");
+  const [timeLeft, setTimeLeft] = useState(getTimeLeft(targetDate));
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setTimeLeft(getTimeLeft(targetDate));
+    }, 1000);
+
+    return () => clearInterval(interval);
+  }, []);
+
+  function getTimeLeft(targetDate) {
+    const now = new Date();
+    const timeDiff = targetDate - now;
+
+    const days = Math.floor(timeDiff / (1000 * 60 * 60 * 24));
+    let hours = Math.floor((timeDiff % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    let minutes = Math.floor((timeDiff % (1000 * 60 * 60)) / (1000 * 60));
+    let seconds = Math.floor((timeDiff % (1000 * 60)) / 1000);
+
+    if (seconds === 0 && minutes > 0) {
+      minutes--;
+      seconds = 59;
+    }
+
+    if (minutes === 0 && hours > 0) {
+      hours--;
+      minutes = 59;
+    }
+
+    return {
+      days,
+      hours,
+      minutes,
+      seconds,
+    };
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="container">
+        <div className="paragraph">
+          <p>Alexanders alldeles egna nedräkning till vad han hoppas ska bli en alldeles kanonbra föräldraledighet.</p>
+        </div>
+        <div className="countdown">
+          <div className="time">{timeLeft.days} days</div>
+          <div className="time">{timeLeft.hours} hours</div>
+          <div className="time">{timeLeft.minutes} minutes</div>
+          <div className="time">{timeLeft.seconds} seconds</div>
+        </div>
+      </div>
     </div>
   );
 }
